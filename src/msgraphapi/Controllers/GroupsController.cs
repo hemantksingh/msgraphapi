d9ce2f8aecd70c4cfdb38c15bfc5b03f9333ba8f
Hemant Kumar <hemantksingh.hk@gmail.com>
2020-08-11 17:48:14 +0100

    Add groups endpoint

---
+++ src/msgraphapi/Controllers/GroupsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json;
+
+namespace msgraphapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IConfidentialClientApplication _clientApplication;
+        private readonly ILogger<UsersController> _logger;
+
+        public GroupsController(IConfidentialClientApplication clientApplication, ILogger<UsersController> logger)
+        {
+            _clientApplication = clientApplication;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                AuthenticationResult authenticationResult = await _clientApplication
+                    .AcquireTokenForClient(new[] { "https://graph.microsoft.com/.default" })
+                    .ExecuteAsync();
+
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+
+                var response = await httpClient.GetAsync("https://graph.microsoft.com/v1.0/groups");
+                var content = await response.Content.ReadAsStringAsync();
+
+                var deserializeObject = JsonConvert.DeserializeObject<dynamic>(content);
+                return Ok(deserializeObject);
+            }
+            catch (MsalServiceException ex) when (ex.Message.Contains("AADSTS50049"))
+            {
+                _logger.LogError(ex, "Incorrect authority configuration");
+                return new ObjectResult($"Incorrect authority configuration, {ex.Message}");
+            }
+        }
+    }
+}
