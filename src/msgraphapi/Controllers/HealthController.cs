7452c86761ad28658ecdc9c7b84c2d8350b751e3
Hemant Kumar <hemantksingh.hk@gmail.com>
2020-10-08 18:52:59 +0100

    Add health controller

---
+++ src/msgraphapi/Controllers/HealthController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace msgraphapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController: ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            foreach (var requestHeader in Request.Headers)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Listing the request headers:");
+                Console.WriteLine($"${requestHeader.Key}: {requestHeader.Value} ");
+            }
+
+            return Ok("Healthy");
+        }
+
+
+    }
+}
